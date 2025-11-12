589257b37de5f906f175dfb3080e01928a1b154b
Anh Thu <hangthianhthu90048@gmail.com>
2025-11-12 10:49:35 +0700

    form đổi mật khẩu

---
+++ PharmacyApp/Forms/FrmChangePassword.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace PharmacyApp.Forms
+{
+    partial class FrmChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.btnMinimize = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.btnClose = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.card = new Guna.UI2.WinForms.Guna2Panel();
+            this.btnCancel = new Guna.UI2.WinForms.Guna2Button();
+            this.lstRules = new System.Windows.Forms.CheckedListBox();
+            this.prgStrength = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.btnSave = new Guna.UI2.WinForms.Guna2Button();
+            this.lblStrength = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.txtConfirm = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtNew = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtOld = new Guna.UI2.WinForms.Guna2TextBox();
+            this.lblTitle = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.card.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimize.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.btnMinimize.FillColor = System.Drawing.Color.Transparent;
+            this.btnMinimize.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.btnMinimize.IconColor = System.Drawing.Color.White;
+            this.btnMinimize.Location = new System.Drawing.Point(681, 12);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(35, 30);
+            this.btnMinimize.TabIndex = 11;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.FillColor = System.Drawing.Color.Transparent;
+            this.btnClose.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.btnClose.IconColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(738, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(35, 30);
+            this.btnClose.TabIndex = 12;
+            // 
+            // card
+            // 
+            this.card.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.card.BackColor = System.Drawing.Color.Transparent;
+            this.card.BorderRadius = 12;
+            this.card.Controls.Add(this.btnCancel);
+            this.card.Controls.Add(this.lstRules);
+            this.card.Controls.Add(this.prgStrength);
+            this.card.Controls.Add(this.btnSave);
+            this.card.Controls.Add(this.lblStrength);
+            this.card.Controls.Add(this.txtConfirm);
+            this.card.Controls.Add(this.txtNew);
+            this.card.Controls.Add(this.txtOld);
+            this.card.Controls.Add(this.lblTitle);
+            this.card.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.card.Location = new System.Drawing.Point(120, 107);
+            this.card.Name = "card";
+            this.card.ShadowDecoration.Enabled = true;
+            this.card.ShadowDecoration.Shadow = new System.Windows.Forms.Padding(0, 6, 0, 12);
+            this.card.Size = new System.Drawing.Size(560, 466);
+            this.card.TabIndex = 13;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.BorderRadius = 6;
+            this.btnCancel.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCancel.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnCancel.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnCancel.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnCancel.FillColor = System.Drawing.Color.Gainsboro;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.DimGray;
+            this.btnCancel.Location = new System.Drawing.Point(90, 398);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 40);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lstRules
+            // 
+            this.lstRules.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lstRules.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lstRules.FormattingEnabled = true;
+            this.lstRules.Items.AddRange(new object[] {
+            "≥ 8 ký tự",
+            "Có chữ HOA",
+            "Có chữ thường",
+            "Có số",
+            "Có ký tự đặc biệt (!@#$...)"});
+            this.lstRules.Location = new System.Drawing.Point(90, 295);
+            this.lstRules.Name = "lstRules";
+            this.lstRules.Size = new System.Drawing.Size(380, 85);
+            this.lstRules.TabIndex = 7;
+            // 
+            // prgStrength
+            // 
+            this.prgStrength.FillColor = System.Drawing.Color.Gainsboro;
+            this.prgStrength.Location = new System.Drawing.Point(90, 275);
+            this.prgStrength.Name = "prgStrength";
+            this.prgStrength.Size = new System.Drawing.Size(380, 8);
+            this.prgStrength.TabIndex = 6;
+            this.prgStrength.Text = "guna2ProgressBar1";
+            this.prgStrength.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            this.prgStrength.Value = 10;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.BorderRadius = 6;
+            this.btnSave.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnSave.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnSave.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnSave.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnSave.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(201)))), ((int)(((byte)(170)))));
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(315, 398);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(180, 40);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblStrength
+            // 
+            this.lblStrength.BackColor = System.Drawing.Color.Transparent;
+            this.lblStrength.Location = new System.Drawing.Point(90, 255);
+            this.lblStrength.Name = "lblStrength";
+            this.lblStrength.Size = new System.Drawing.Size(86, 18);
+            this.lblStrength.TabIndex = 5;
+            this.lblStrength.Text = "Độ mạnh: Yếu";
+            // 
+            // txtConfirm
+            // 
+            this.txtConfirm.BorderRadius = 6;
+            this.txtConfirm.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtConfirm.DefaultText = "";
+            this.txtConfirm.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtConfirm.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtConfirm.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtConfirm.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtConfirm.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtConfirm.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirm.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtConfirm.Location = new System.Drawing.Point(90, 192);
+            this.txtConfirm.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.txtConfirm.Name = "txtConfirm";
+            this.txtConfirm.PasswordChar = '●';
+            this.txtConfirm.PlaceholderText = "Nhập lại mật khẩu mới";
+            this.txtConfirm.SelectedText = "";
+            this.txtConfirm.Size = new System.Drawing.Size(405, 48);
+            this.txtConfirm.TabIndex = 2;
+            this.txtConfirm.TextChanged += new System.EventHandler(this.txtConfirm_TextChanged);
+            // 
+            // txtNew
+            // 
+            this.txtNew.BorderRadius = 6;
+            this.txtNew.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNew.DefaultText = "";
+            this.txtNew.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtNew.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtNew.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtNew.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtNew.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtNew.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNew.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtNew.Location = new System.Drawing.Point(90, 136);
+            this.txtNew.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.txtNew.Name = "txtNew";
+            this.txtNew.PasswordChar = '●';
+            this.txtNew.PlaceholderText = "Mật khẩu mới";
+            this.txtNew.SelectedText = "";
+            this.txtNew.Size = new System.Drawing.Size(405, 48);
+            this.txtNew.TabIndex = 1;
+            this.txtNew.TextChanged += new System.EventHandler(this.txtNew_TextChanged);
+            // 
+            // txtOld
+            // 
+            this.txtOld.BorderRadius = 6;
+            this.txtOld.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtOld.DefaultText = "";
+            this.txtOld.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtOld.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtOld.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtOld.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtOld.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtOld.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOld.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtOld.Location = new System.Drawing.Point(90, 80);
+            this.txtOld.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.txtOld.Name = "txtOld";
+            this.txtOld.PasswordChar = '●';
+            this.txtOld.PlaceholderText = "Mật khẩu hiện tại";
+            this.txtOld.SelectedText = "";
+            this.txtOld.Size = new System.Drawing.Size(405, 48);
+            this.txtOld.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(215)))), ((int)(((byte)(183)))));
+            this.lblTitle.Location = new System.Drawing.Point(169, 17);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(223, 52);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Đổi mật khẩu";
+            this.lblTitle.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(22)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(800, 652);
+            this.Controls.Add(this.card);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnMinimize);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmChangePassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmChangePassword";
+            this.card.ResumeLayout(false);
+            this.card.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2ControlBox btnMinimize;
+        private Guna.UI2.WinForms.Guna2ControlBox btnClose;
+        private Guna.UI2.WinForms.Guna2Panel card;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblTitle;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblStrength;
+        private Guna.UI2.WinForms.Guna2TextBox txtConfirm;
+        private Guna.UI2.WinForms.Guna2TextBox txtNew;
+        private Guna.UI2.WinForms.Guna2TextBox txtOld;
+        private System.Windows.Forms.CheckedListBox lstRules;
+        private Guna.UI2.WinForms.Guna2ProgressBar prgStrength;
+        private Guna.UI2.WinForms.Guna2Button btnSave;
+        private Guna.UI2.WinForms.Guna2Button btnCancel;
+    }
+}
